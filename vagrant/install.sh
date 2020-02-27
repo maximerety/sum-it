77d1190be024fc095176326f4acc7122808efbb2
maximerety <maxime.rety@tanker.io>
2020-02-27 09:59:25 +0100

    Upgrade Ruby and Node.js dependencies

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -2,8 +2,8 @@
 set -x
 
 # software versioning
-NODEJS_VERSION=10.16.3
-RUBY_VERSION=2.6.3
+NODEJS_VERSION=12.16.1
+RUBY_VERSION=2.6.5
 RUBY_VERSION_SHORT=2.6
 
 # repository index update
@@ -25,7 +25,7 @@ wget -q https://nodejs.org/dist/v$NODEJS_VERSION/node-v$NODEJS_VERSION-linux-x64
 rm -rf node-v$NODEJS_VERSION-linux-x64*
 
 # install ruby dependencies on debian
-# (stolen from RVM source: https://github.com/rvm/rvm/blob/0e3b23a/scripts/functions/requirements/debian)
+# (stolen from RVM source: https://github.com/rvm/rvm/blob/28bf567/scripts/functions/requirements/debian)
 sudo apt-get -y install make
 sudo apt-get -y install g++ gcc
 sudo apt-get -y install autoconf automake bison ca-certificates curl libc6-dev libffi-dev libgdbm-dev libncurses5-dev \
@@ -71,11 +71,3 @@ sudo npm install gulp-cli -g
 
 # install locally the gulp modules listed in the package.json
 npm install
-# or (if no package.json yet):
-# npm init
-# npm install --save-dev gulp
-# npm install --save-dev gulp-babel @babel/core @babel/preset-env
-# npm install --save-dev gulp-concat
-# npm install --save-dev gulp-minify
-# npm install --save-dev gulp-header
-# npm install --save-dev gulp-file-include

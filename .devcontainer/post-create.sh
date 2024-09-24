486dc10deb8fde1c6c76f3de289deaee3b1bb6ea
maximerety <maxime.rety@gmail.com>
2024-09-24 13:00:56 +0200

    Upgrade Ruby and Node.js versions and dependencies
    * Ruby 3.2 -> 3.3
    * Node.js 20 -> 22
    * Gulp 4.0.2 -> 5.0.0
    * Regenerate .lock files with latest compatible deps

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-# Install Node.js 20
+# Install Node.js 22
 export NVM_DIR="/usr/local/share/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-nvm install 20 --default
+nvm install 22 --default
 
 set -e
 
@@ -20,3 +20,9 @@ npm install
 
 # Install the Ruby gems specified by the project
 bundle install
+
+# Workaround for the following issue:
+#   fatal: detected dubious ownership in repository at '/workspaces/sum-it'
+#
+# The ~/.gitconfig file will only be modified on the guest container, not on the host.
+git config --global --add safe.directory /workspaces/sum-it
